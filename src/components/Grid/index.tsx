6370869ff63be005a8b27e4e6bf544605dcb7367
Francisco Miguel Villar Salvador <fvi@novatec-gmbh.de>
2024-05-15 00:25:13 +0200

    type grid component props

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,4 +1,8 @@
-const Grid = ({ children }) => {
+type GridProps = {
+  children: JSX.Element;
+};
+
+const Grid = ({ children }: GridProps) => {
   return (
     <ul className='grid gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
       {children}
